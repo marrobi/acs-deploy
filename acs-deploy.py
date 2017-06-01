576ff13b88f67580ce8ab7a2095324ef8a1b8755
Marcus Robinson <marrobi@microsoft.com>
2017-06-01 10:14:34 +0200

    latest script

--- acs-deploy.py
+++ acs-deploy.py
@@ -11,16 +11,21 @@ def invoke_sub_process(command):
     print "Executing: " + command
 
     try:
-        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-    except:
-        "Exception when executing the command '" + command + "'"
+        process = subprocess.check_output(command.split())
+        print "Output: " + process
+        
+    except subprocess.CalledProcessError as e:
+        print e.output
+        sys.exit(1)
+  
+  
+    # No exception but possible stderr error
+    if process.find('error') > -1:
+        print "Exception when executing the command: '" + process + "'"
+        sys.exit(1)
     else:
-        # No exception but possible stderr error
-        if error is not None:
-            print str(error)
-            exit()
-    return output
+        return process
+    
 
 def read_config(filepath):
     print "Reading config file " + deployment_config_file 
@@ -37,15 +42,15 @@ def replace_tokens(filepath, cfg):
     # TODO: valid config values
     dns_prefix = cfg['dns_prefix']
     
-    if dns_prefix != ''.join(i for i in dns_prefix if not i.isdigit()):
-        print "Config value 'dns_prefix' can only contain alphabetical characters"
-        exit()
-    if len(dns_prefix) > 8:
-        print "Config value 'dns_prefix' is too long, max 8 chars"
-        exit()
+    #if dns_prefix != ''.join(i for i in dns_prefix if not i.isdigit()):
+    #    print "Config value 'dns_prefix', '" + dns_prefix + "' can only contain alphabetical characters"
+    #    sys.exit(1)
+    if len(dns_prefix) > 45:
+        print "Config value 'dns_prefix', '" + dns_prefix + "' is too long, max 16 chars"
+        sys.exit(1)
 
-    ssh = invoke_sub_process("more " + cfg['SSHPATH'] + "/id_rsa.pub")
-    ssh = str(ssh).strip(' ').replace("\n", "")
+   
+  
 
     # replace the target tokens with config values
     filedata = filedata.replace("\"__MASTERCOUNT__\"", str(cfg['master_count']))
@@ -55,7 +60,7 @@ def replace_tokens(filepath, cfg):
     filedata = filedata.replace( "__AGENTPOOLNAME__", cfg['agent_poolname'])
     filedata = filedata.replace( "__AGENTVMSIZE__", cfg['agent_vmsize'])
     filedata = filedata.replace( "__ADMINUSERNAME__", cfg['admin_username'])
-    filedata = filedata.replace( "__SSHPUBKEY__", str(ssh))
+    filedata = filedata.replace( "__SSHPUBKEY__",cfg['ssh_pub_key'])
     filedata = filedata.replace( "__SERVICEPRINCIPALAPPKEY__", cfg['service_principal_app_id'])
     filedata = filedata.replace( "__SERVICEPRINCIPALSECRET__", cfg['service_principal_password'])
 
@@ -72,7 +77,7 @@ def check_dependencies():
     print "Checking platform support"
     if(platform != "linux" and platform != "linux2"):
         print "This script only works on the Linux platform"
-        exit()
+        sys.exit(1)
     else:
         print "Success, supported platform"
 
@@ -86,57 +91,151 @@ def check_dependencies():
     file_exists = os.path.isfile("cluster-definition-template.json") 
     if file_exists == False:
         print "Cluster definition template does not exist!"
-        exit()
+        sys.exit(1)
     print "Success, cluster definition file exists"
 
-    print "Checking public SSH key is mounted"
-    home = os.environ['HOME']
-    file_exists = os.path.isfile(home + "/.ssh/id_rsa.pub")
-    if file_exists == False:
-        print "Public SSH key was not mounted correctly!"
-        exit()
-    print "Success, public SSH key mounted"
+  
+def get_config_args():
+        # use individual args
+        import getopt
+        cfg = {}
+
+        # check
+        opts, args = getopt.getopt(sys.argv[1:],'',['master_count=','master_vmsize=','dns_prefix=','agent_count=','agent_poolname=','agent_vmsize=','admin_username=','service_principal_app_id=','service_principal_password=','ssh_pub_key=','cluster_definition_template_url=','service_principal_name=','tenant=','resource_group_name=','resource_group_location=','deployment_name=','parameters='] )
+        
+        
+        for opt, arg in opts:
+            try:
+                if opt == "--master_count":
+                    cfg['master_count'] =  arg
+                elif opt == "--master_vmsize":
+                    cfg['master_vmsize'] =  arg
+                elif opt == "--dns_prefix":
+                    cfg['dns_prefix'] =  arg
+                elif opt == "--agent_count":
+                    cfg['agent_count'] =  arg
+                elif opt == "--agent_poolname":
+                    cfg['agent_poolname'] =  arg
+                elif opt == "--agent_vmsize":
+                    cfg['agent_vmsize'] =  arg
+                elif opt == "--admin_username":
+                    cfg['admin_username'] =  arg
+                elif opt == "--service_principal_name":
+                    cfg['service_principal_name'] =  arg
+                elif opt == "--service_principal_app_id":
+                    cfg['service_principal_app_id'] =  arg
+                elif opt == "--service_principal_password":
+                    cfg['service_principal_password'] =  arg    
+                elif opt == "--resource_group_location":
+                    cfg['resource_group_location'] =  arg  
+                elif opt == "--resource_group_name":
+                    cfg['resource_group_name'] =  arg 
+                elif opt == "--deployment_name":
+                    cfg['deployment_name'] =  arg      
+                elif opt == "--tenant":
+                    cfg['tenant'] =  arg 
+                elif opt == "--parameters":
+                    cfg['parameters'] =  arg    
+                elif opt == "--ssh_pub_key":
+                    cfg['ssh_pub_key'] =  arg
+                elif opt == "--cluster_definition_template_url":
+                    cfg['cluster_definition_template_url'] =  arg
+                    print "Set cluster_definition_template_url = " + arg
+            except Exception, Argument:    
+                # Error with arguments
+                print "Error with arguments: ",  Argument
+                sys.exit(1)
+
+   
+        return cfg
+
 
 # start script...
-check_dependencies()
+# temp check_dependencies()
 
 print "....."
 print "Starting Azure Container Service Deployment"
 print "....."
 
 deployment_config_file  = None
-
+    
 # set the config via...
-if len(sys.argv) > 1:
+if 'acs_config_path' in sys.argv:
     # command line argument
-    deployment_config_file = sys.argv[1]
+    print "Retrieving config from  path from arguments"
+    deployment_config_file = sys.argv['acs_config_path']
+    config = read_config(deployment_config_file)
+elif 'acs_config_path' in os.environ:
+       # environment variable
+    print "Retrieving config from  path environment var"
+    deployment_config_file = str(os.environ['acs_config_path'])
+    config = read_config(deployment_config_file)
 else:
-    try:
-        # enviroment variable
-        deployment_config_file = str(os.environ['ACS_CONFIG_PATH'])
-    except:
-        # default
-        deployment_config_file = "acs-deploy-config.json"
+    print "Retrieving config from arguments"
+    config = get_config_args()
+    print "config:" + config['master_count']
 
-config = read_config(deployment_config_file)
-root_cluster_definition_name = "cluster-definition"
+# download provided cluster definition file
+print config['cluster_definition_template_url']
+
+if 'cluster_definition_template_url' in config:
+    
+    # download file
+    import urllib
+    testfile = urllib.URLopener()
+    testfile.retrieve(config['cluster_definition_template_url'], "custom-cluster-definition-template.json")
+    root_cluster_definition_name = "custom-cluster-definition"
+else:
+    root_cluster_definition_name = "cluster-definition"
 
 # initialise vars
 print ""
-config['GOPATH'] = os.environ['GOPATH']
-print "GOPATH=" + config['GOPATH']
+#config['GOPATH'] = os.environ['GOPATH']
+#print "GOPATH=" + config['GOPATH']
 config['HOME'] = os.environ['HOME']
-config['SSHPATH'] = config['HOME'] + "/.ssh/"
-print "SSHPATH=" + config['SSHPATH']
+
 config['CWD'] = os.getcwd()
 print "CWD=" + config['CWD']
-print ""
+ 
+
+# handle SSH key
+if 'ssh_pub_key' in config:
+       # ssh as env
+        print "Found  SSH public key value"
+      
+else:
+        print "Checking public SSH key is mounted"
+        home = os.environ['HOME']
+          
+        file_exists = os.path.isfile(home + "/.ssh/id_rsa.pub")
+        if file_exists == False:
+            print "Public SSH key was not mounted correctly!"
+            sys.exit(1)
+        print "Success, public SSH key mounted"
+
+        config['SSHPATH'] = home + "/.ssh/"
+        print "SSHPATH=" + config['SSHPATH']
+      
+        # ssh from file
+        ssh = invoke_sub_process("more " + config['SSHPATH'] + "/id_rsa.pub")
+        ssh = str(ssh).strip(' ').replace("\n", "")
+        config['ssh_pub_key'] = str(ssh)
+
+
 
 print "Replacing tokens in config file"
 replace_tokens(root_cluster_definition_name, config)
 
 print "Executing acs-engine with custom cluster definition"
-invoke_sub_process("acs-engine " + root_cluster_definition_name + ".json" + " --caCertificatePath " + config['SSHPATH'])
+if  'SSHPATH' in config:
+    # use existing certs
+    print "Using existing certs"
+    invoke_sub_process("./acs-engine generate " + root_cluster_definition_name + ".json" + " --caCertificatePath " + config['SSHPATH'])
+else:
+    print "Will create new certs"
+    # create new certs, note: redeploy will fail as certs will change
+    invoke_sub_process("./acs-engine generate " + root_cluster_definition_name + ".json")
+
 
 print "Login to azure"
 invoke_sub_process("az login --service-principal -u " + config['service_principal_name'] + " -p " + config['service_principal_password'] + " --tenant " + config['tenant'])
@@ -146,7 +245,17 @@ invoke_sub_process("az group create --name " + config['resource_group_name'] + "
 
 print "Creating azure container service deployment"
 latest_definition = max(glob.glob(os.path.join('_output/', '*/')), key=os.path.getmtime)
-invoke_sub_process("az group deployment create --name " + config['deployment_name'] + " --resource-group " + config['resource_group_name'] + " --template-file " + "./" + latest_definition + "/azuredeploy.json" + " --parameters " + "@./" + latest_definition + "/azuredeploy.parameters.json")
+
+parameters = " --parameters " + "@./" + latest_definition + "/azuredeploy.parameters.json"
+
+if 'parameters' in config:
+
+    parameters = " --parameters " + "@./" + latest_definition + "/azuredeploy.parameters.json" + " --parameters \"" + config['parameters'] + "\""
+
+
+
+
+invoke_sub_process("az group deployment create --name " + config['deployment_name'] + " --resource-group " + config['resource_group_name'] + " --template-file " + "./" + latest_definition + "/azuredeploy.json" + parameters)
 
 print "Validate resource group exists in azure"
 invoke_sub_process("az group exists --name " + config['resource_group_name'])
@@ -155,14 +264,18 @@ fqdn = config['dns_prefix'] + "." + config['resource_group_location'] + ".clouda
 connection_string = config['admin_username'] + "@" + fqdn
 
 print "Get cluster configuration from master node"
-invoke_sub_process("scp -oStrictHostKeyChecking=no " + connection_string + ":.kube/config .")
-os.environ['KUBECONFIG'] = config['CWD'] + "/config"
+output = invoke_sub_process("scp -oStrictHostKeyChecking=no " + connection_string +       ":.kube/config ~/.kube/config")
+print "SCP Result: "  + output
+
+if len(output) > 0:
+    print "Failed to retrieve kubeconfig"
+    sys.exit(1)    
 
 print "Running cluster validation tests"
-process = subprocess.Popen("kubectl cluster-info | grep -o error | wc -l", stdout=subprocess.PIPE, shell=True)
-num_errors = int(process.communicate()[0])
+process = subprocess.check_output("kubectl cluster-info")
 
-if(num_errors > 0):
+
+if process.find('error') > -1:
     print "Cluster is alive but not all services look are healthy"
 else:
     print "Cluster is alive and all services look healthy"
